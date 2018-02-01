f47bbe89cba57ce61a365419fdf7424a58238668
Pietro Gori <pietro_gori@hotmail.com>
2018-02-01 00:51:30 -0200

    Aula 04 - Isolando responsabilidades com Helpers, e impedindo instâncias de classes com um throw no construtor

---
+++ client/js/app/helpers/DateHelper.js
@@ -0,0 +1,19 @@
+class DateHelper{
+    constructor(){
+        throw new Error('DateHelper não pode ser instanciada');
+    }
+    
+    static dataParaTexto(data){
+        return `${data.getData()}
+        /${data.getMonth()+1}
+        /${data.getFullYear()}`;
+    }
+
+    static textoParaData(texto){
+        if(!/^\d{4}-\d{2}-\d{2}/.test(texto))
+            throw new Error('Deve estar no formato aaaa-mm-dd');
+        return new Date(
+            ...texto.split('-').map((item, index) => item - index % 2)
+        );
+    }
+}
